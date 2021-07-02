4b77d5fabc7122c91be4c035bff36da93f183d76
Your Name <you@example.com>
2021-07-02 17:43:41 +0900

    양방향 매핑

--- src/main/java/com/korea/dbapp/domain/user/User.java
+++ src/main/java/com/korea/dbapp/domain/user/User.java
@@ -1,10 +1,16 @@
 package com.korea.dbapp.domain.user;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.korea.dbapp.domain.post.Post;
 
 @Entity
 public class User {
@@ -19,9 +25,25 @@ public class User {
 	private String email;
 	private String address;
 	
+	// 여기에 post 적으려면 컬렉션으로 들어가야함
+	//user - one , post - many
+	@JsonIgnoreProperties({"user", "title"}) // user만 json으로 파싱하지 말라는 뜻
+	@OneToMany(mappedBy = "user") // mappedBy = "post의 User 타입의 변수 이름" ==> posts가 외래키가 아니라고 알려줌
+	private List<Post> posts;
+	// id가 1일때 post에 user_id가 1인 것들을 조인해서 다 들고와줌 
+	// 하지만 @OneToMany때문에 외래키가 만들어지는데 그렇게 되면 안됨!
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
 	public String getAddress() {
 		return address;
 	}
+	
 	public void setAddress(String address) {
 		this.address = address;
 	}
